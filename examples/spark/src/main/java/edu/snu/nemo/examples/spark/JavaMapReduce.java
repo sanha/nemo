949fe70a6e2f3210d4d50013db53d2befce49a14
sanha <sanhaleehana@naver.com>
2018-04-30 13:54:10 +0900

    no kryo

--- examples/spark/src/main/java/edu/snu/nemo/examples/spark/JavaMapReduce.java
+++ examples/spark/src/main/java/edu/snu/nemo/examples/spark/JavaMapReduce.java
@@ -48,7 +48,7 @@ public final class JavaMapReduce {
     final SparkSession spark = SparkSession
         .builder()
         .config("mapreduce.input.fileinputformat.input.dir.recursive", "true")
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        //.config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
         .appName("JavaWordCount")
         .getOrCreate();
 
