184fdf86f0dfebf75237321b948138ef917c5ef5
sanha <sanhaleehana@naver.com>
2018-10-30 15:04:33 +0900

    John's key extractor remedy

--- compiler/frontend/beam/src/main/java/org/apache/nemo/compiler/frontend/beam/BeamKeyExtractor.java
+++ compiler/frontend/beam/src/main/java/org/apache/nemo/compiler/frontend/beam/BeamKeyExtractor.java
@@ -15,8 +15,11 @@
  */
 package org.apache.nemo.compiler.frontend.beam;
 
+import org.apache.beam.sdk.values.Row;
 import org.apache.nemo.common.KeyExtractor;
 import org.apache.beam.sdk.values.KV;
+
+import java.util.Arrays;
 
 /**
  * Extracts the key from a KV element.
@@ -28,7 +31,14 @@ final class BeamKeyExtractor implements KeyExtractor {
     if (element instanceof KV) {
       // Handle null keys, since Beam allows KV with null keys.
       final Object key = ((KV) element).getKey();
-      return key == null ? 0 : key;
+      if (key == null) {
+        return 0;
+      } else if (key instanceof Row) {
+        // TODO #223: Use Row.hashCode in BeamKeyExtractor
+        return Arrays.hashCode(((Row) key).getValues().toArray());
+      } else {
+        return key;
+      }
     } else {
       return element;
     }
