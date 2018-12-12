4bbe1b6bd1a97481e67b5cff9625f3edb74c5408
sanha <sanhaleehana@naver.com>
2018-12-12 15:46:12 +0900

    Print unopt dist

--- runtime/common/src/main/java/org/apache/nemo/runtime/common/optimizer/pass/runtime/DataSkewRuntimePass.java
+++ runtime/common/src/main/java/org/apache/nemo/runtime/common/optimizer/pass/runtime/DataSkewRuntimePass.java
@@ -94,7 +94,7 @@ public final class DataSkewRuntimePass extends RuntimePass<Pair<StageEdge, Map<O
     // Calculate keyRanges.
     final List<KeyRange> keyRanges = calculateKeyRanges(metricData.right(), dstParallelism, hashRange);
 
-    //printUnOpimizedDist(metricData.right(), dstParallelism, targetEdge.getId());
+    printUnOpimizedDist(metricData.right(), dstParallelism, targetEdge.getId());
     //printOpimizedDist(metricData.right(), hashRange, keyRanges, targetEdge.getId());
 
     //LOG.info("Optimized key ranges: " + keyRanges);
